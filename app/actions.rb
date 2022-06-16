aced3785e5e7c7f5c8ed19373323a672df200eca
SummGutt94 <r_clegg_94@outlook.com>
2022-06-16 03:30:33 +0000

    add get and post actions for '/signup' path

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,27 @@
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
-  p @finstagram_posts
-  erb(:index)
+    erb(:index)
+end
+
+get '/signup' do
+  @user = User.new
+  erb(:signup)
+
+end
+
+post '/signup' do     # if a user navigates to the path "/signup",
+  
+  # grab user input values from params
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
 end
